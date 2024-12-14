e5f5691fc32ad1939de26ed222bb9013adf77e61
nikumu <andrecarlosbb@gmail.com>
2024-12-14 17:51:54 -0300

    init

--- src/database/migrations/20241214194022-create-users.js
+++ src/database/migrations/20241214194022-create-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -36,11 +36,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      createdAt: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updatedAt: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const {Router} = require('express');
+const { Router }  = require('express');
+const UserModel = require('./apps/models/Users');
 
 const routes = new Router();
 
-routes.get('/health', (req, res) => {
-    return res.send({message: 'Connected with success!'});
+routes.get('/users', async (req, res) => {
+    const allUsers = await UserModel.findAll();
+    res.send({ users: allUsers });
 });
+
+routes.get('/health', (req, res) =>  res.send({
+    message: 'Connected with success!',
+}));
 
 module.exports = routes;
