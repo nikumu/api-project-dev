762443854c4c6e23ed49c8226593a47457162259
nikumu <andrecarlosbb@gmail.com>
2024-12-14 15:23:43 -0300

    database

---
+++ src/configs/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
+};
