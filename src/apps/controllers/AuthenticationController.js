cb7d1dcf86cc41bdd5693b08110b2aaf96501cd4
nikumu <andrecarlosbb@gmail.com>
2024-12-15 15:54:11 -0300

    AuthSchema

--- src/apps/controllers/AuthenticationController.js
+++ src/apps/controllers/AuthenticationController.js
@@ -33,8 +33,8 @@ class AuthenticationController {
 
         const newId = `${iv}:${content}`;
 
-        const token = jwt.sign({ newId }, process.env.HASH_BCRYPT, {
-            expiresIn: '7d',
+        const token = jwt.sign({ userId: newId }, process.env.HASH_BCRYPT, {
+            expiresIn: process.env.EXPIRE_IN,
         });
 
         return res.status(200).json({ user: { id, user_name: userName }, token });
