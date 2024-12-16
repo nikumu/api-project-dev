1bf5d09bf7b57c5ccf6df6203b712673eab0acb0
nikumu <andrecarlosbb@gmail.com>
2024-12-16 16:47:09 -0300

    route to delete post

--- src/apps/controllers/PostController.js
+++ src/apps/controllers/PostController.js
@@ -16,6 +16,39 @@ class PostController {
 
         return res.status(200).json({ data: {image, description} });
     }
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const verifyPost = await Posts.findOne({
+            where: {
+                id,
+
+            },
+        });
+
+        if (!verifyPost) {
+            return res.status(404).json({ message: 'post does not exits!' });
+        }
+
+        if (verifyPost.author_id != req.userId) {
+            return res.status(401).json({
+                message: 'You don\'t have permission to delete this post! ',
+            });
+        } 
+
+        const deletePost = await Posts.destroy({
+            where: {
+                id,
+            },
+        });
+
+        if (!deletePost) {
+            return res.status(400).json({ message: 'Failed to delete this post! '});
+        }
+
+        return res.status(200).json({ message: 'Post deleted! '});
+    }
 }
 
 module.exports = new PostController();
--- src/routes.js
+++ src/routes.js
@@ -32,5 +32,6 @@ routes.get('/user-profile', UserController.userProfile);
 
 routes.post('/upload', upload.single('image'), FileController.upload);
 
-routes.post('/posts', schemaValidator(postSchema), PostController.create);
+routes.post('/post', schemaValidator(postSchema), PostController.create);
+routes.delete('/delete-post/:id', PostController.delete);
 module.exports = routes; 
